654c75aa7e205fb14f2ddcf0ec1e69773e536995
simon245 <simonball245@gmail.com>
2015-08-28 12:34:40 +0800

    added add to cart button

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,6 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
-    @time_now = Time.now.strftime('%H:%M %d/%m/%y ')
+    @time_now = Time.now.strftime('%H:%M %d/%m/%y')
   end
 end
