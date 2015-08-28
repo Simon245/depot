26d6b9c5f768c0e358db7e531259272586dd8f49
simon245 <simonball245@gmail.com>
2015-08-28 14:52:28 +0800

    add session counter for visited and reset when item added to cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,14 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    increment_counter
     @time_now = Time.now.strftime('%H:%M %d/%m/%y')
+  end
+
+  def increment_counter
+    @counter = session[:counter]
+    #ternery operator: if @counter is nil then @counter = 1 else @counter += 1
+    @counter.nil? ? @counter = 1 : @counter += 1
+    session[:counter] = @counter
   end
 end
